025830cd700c245054356bc112be826ba430324b
sahawut <sahawut@gmail.com>
2017-10-19 14:25:12 -0400

    - Evolved to MIDAS-SSO. - Showed some information about the logged in user. - Removed unnecessary code about auth0js.parseHash. - Filled the constants about the OIDC SSO Example client. - Added a link to sign off from MIDAS-SSO.

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,5 +1,6 @@
-const AUTH0_CLIENT_ID = '';
-const AUTH0_DOMAIN = '';
+const AUTH0_CLIENT_ID = 'KypnNzj458749ZsOaKzn7QL1SFhdyP4S';
+const AUTH0_DOMAIN = 'midas-dev-sso.auth0.com';
+const MIDAS_ACCOUNTS_URL = 'https://betaweb.rods.pitt.edu/hub-alpha';
 
 if (!AUTH0_CLIENT_ID || !AUTH0_DOMAIN) {
     alert('Make sure to set the AUTH0_CLIENT_ID and AUTH0_DOMAIN variables in auth0-variables.js.');
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 function displayStatus() {
-    let status;
+    var status;
     const token = localStorage.getItem('accessToken');
     const expirationDate = new Date(Number.parseInt(localStorage.getItem('expirationDate')));
     const isExpired = expirationDate < new Date();
+    const profile = localStorage.getItem('profile');
+
 
     if (!token) {
+        if (localStorage.getItem('sso')){
+            renew();
+        }
         status = 'There is no access token present in local storage, meaning that you are not logged in. <a href="#" onclick="renew()">Click here to attempt an SSO login</a>';
     } else if (isExpired) {
         status = 'There is an expired access token in local storage. <a href="#" onclick="renew()">Click here to renew it</a>';
@@ -13,13 +18,23 @@ function displayStatus() {
         status = `There is an access token in local storage, and it expires on ${expirationDate}. <a href="#" onclick="renew()">Click here to renew it</a>`;
         document.getElementById('logout').style.visibility = 'visible';
     }
+    if (profile) {
+        const user = JSON.parse(profile);
+        status += `<p>Hello ${user.name}</p>
+                    <image src="${user.picture}" alt="You don't have any picture"></image>
+                    <p>Roles: ${user['https://epimodels.org/app_metadata'].roles}</p>
+        `;
+    }
     document.getElementById('status').innerHTML = status;
 }
 
 function saveAuthResult (result) {
     localStorage.setItem('accessToken', result.accessToken);
     localStorage.setItem('expirationDate', Date.now() + Number.parseInt(result.expiresIn) * 1000);
-    displayStatus();
+    auth0js.client.userInfo(result.accessToken, function(err, user) {
+        localStorage.setItem('profile', JSON.stringify(user));
+        displayStatus();
+    });
 }
 
 function renew () {
@@ -27,21 +42,35 @@ function renew () {
         redirectUri: 'http://localhost:3000/callback.html',
         usePostMessage: true
     }, function (err, result) {
-        if (err) {
+        if (err || (result && result.error)) { // For auth0.js version 8.8, the error shows up at result.error
+            if (result)
+                err = {error: result.errorDescription}; // For auth0.js version 8.8, the error message shows up at result.errorDescription
             alert(`Could not get a new token using silent authentication (${err.error}). Redirecting to login page...`);
-            auth0js.authorize();
+            // auth0js.authorize();
+            signOnViaMidasAccounts();
         } else {
             saveAuthResult(result);
         }
     });
 }
 
-auth0js.parseHash(window.location.hash, function (err, result) {
-    if (err) {
-        console.error(err);
-    } else if (result) {
-        saveAuthResult(result);
-    }
-});
 
 displayStatus();
+
+
+function signOnViaMidasAccounts() {
+    localStorage.setItem('sso', true);
+    window.location.href = toMidasAccountsUrl('/sso');
+}
+
+function signoff() {
+    localStorage.clear();
+    window.location.href = toMidasAccountsUrl('/logout');
+}
+
+function toMidasAccountsUrl(endpoint, message, title) {
+    title = title || "OIDC SSO Example";
+    message = message || "Please login to use the services";
+    return MIDAS_ACCOUNTS_URL + endpoint + '?returnToUrl='
+        + encodeURIComponent(window.location) + '&title=' + title + '&message=' + message;
+}
